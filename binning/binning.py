0d1a8dfb3db507040ee5f422ec0b8cee0f58db7b
Shreyas Bandekar <shreyasbandekar01@gmail.com>
2026-03-11 20:25:28 +0530

    Update solution for binning

--- binning/binning.py
+++ binning/binning.py
@@ -14,10 +14,8 @@ def binning(values, num_bins):
         
     bin_width = range_val / num_bins
 
-    # Calculate indices, floor them, and clip to ensure max value stays in bounds
     index = (values - min_val) / bin_width
     bins = np.floor(index).astype(int)
     bins = np.clip(bins, 0, num_bins - 1)
-    
-    # Convert the NumPy array back to a standard Python list
+ 
     return bins.tolist()
